5e95a9c39c3f7fae90e1bf5a4934280e26982756
Bruno Sabino <bruno.p.sabino@gmail.com>
2019-07-15 23:03:33 -0300

    Consumir dados da Api

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,36 +1,84 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div>
-    <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div className="container">
-        <div className="navbar-header page-scroll">
-          <a className="navbar-brand page-scroll" href="#page-top">
-              <img src="images/logo.png" height="30" />
-          </a>
-        </div>
-        <div className="collapse navbar-collapse navbar-ex1-collapse">
-          <ul className="nav navbar-nav">
-            <li>
-              <a href="">Menu item</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <section id="intro" className="intro-section">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <h1><img src="images/logo.png" /></h1>
-            <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+import api from './Api'
+
+
+class App extends React.Component {
+    constructor(props){
+      super(props)
+
+      this.state = {
+        genres: [],
+        isLoading: false
+      }
+    }
+
+    componentDidMount(){
+      this.setState({isLoading: true})
+      api.loadGenres()
+        .then((res) =>{
+          this.setState({
+            isLoading: false,
+            genres: res.data
+          })
+        })
+    }
+     
+    renderGenreLink(genre){
+      return(
+        <span>&nbsp;<a href=''>{genre}</a></span>
+        
+      )
+    }
+
+    render(){
+      return (
+        <div>
+        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header page-scroll">
+              <a className="navbar-brand page-scroll" href="#page-top">
+                  <img src="images/logo.png" height="30" />
+              </a>
+            </div>
+            <div className="collapse navbar-collapse navbar-ex1-collapse">
+              <ul className="nav navbar-nav">
+                <li>
+                  <a href="">Menu item</a>
+                </li>
+              </ul>
+            </div>
           </div>
+        </nav>
+        <section id="intro" className="intro-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <h1><img src="images/logo.png" /></h1>
+                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          {
+            this.state.isLoading &&
+            <span>Carregando, por favor aguarde...</span>
+          } 
+          {
+            !this.state.isLoading &&
+            <div>
+              Ver séries do gênero:
+              {this.state.genres.map(this.renderGenreLink)}
+            </div>
+          }
+        </section>
+
         </div>
-      </div>
-    </section>
-    </div>
-  );
+      )
+    }
+  
 }
 
 export default App;
