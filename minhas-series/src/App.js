b4b99c7bfea71a66fa55e6708ab9c2c9d85a3af5
Bruno Sabino <bruno.p.sabino@gmail.com>
2019-07-17 00:28:50 -0300

    React Router

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,79 +1,45 @@
 import React, {Component} from 'react'
-import api from './Api'
+import {BrowserRouter as Router,
+        Route,
+        Link } from 'react-router-dom'
 
+import Home from './Home'
+
+//Functional Stateless Component
+const About = () => <section className="intro-section"><h1>Sobre</h1></section>
 
 class App extends React.Component {
-    constructor(props){
-      super(props)
-
-      this.state = {
-        genres: [],
-        isLoading: false
-      }
-    }
-
-    componentDidMount(){
-      this.setState({isLoading: true})
-      api.loadGenres()
-        .then((res) =>{
-          this.setState({
-            isLoading: false,
-            genres: res.data
-          })
-        })
-    }
-     
-    renderGenreLink(genre){
-      return(
-        <span>&nbsp;<a href=''>{genre}</a></span>
-        
-      )
-    }
 
     render(){
       return (
-        <div>
-        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-          <div className="container">
-            <div className="navbar-header page-scroll">
-              <a className="navbar-brand page-scroll" href="#page-top">
-                  <img src="images/logo.png" height="30" />
-              </a>
-            </div>
-            <div className="collapse navbar-collapse navbar-ex1-collapse">
-              <ul className="nav navbar-nav">
-                <li>
-                  <a href="">Menu item</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <section id="intro" className="intro-section">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <h1><img src="images/logo.png" /></h1>
-                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+        <Router>
+          <div>
+            <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+              <div className="container">
+                <div className="navbar-header page-scroll">
+                  <a className="navbar-brand page-scroll" href="#page-top">
+                      <img src="images/logo.png" height="30" />
+                  </a>
+                </div>
+                <div className="collapse navbar-collapse navbar-ex1-collapse">
+                  <ul className="nav navbar-nav">
+                    <li>
+                      <Link to="/">Início</Link>
+                    </li>
+                    <li>
+                      <Link to="/new">Nova Série</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">Sobre</Link>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
+            </nav>
           </div>
-        </section>
-        <section>
-          {
-            this.state.isLoading &&
-            <span>Carregando, por favor aguarde...</span>
-          } 
-          {
-            !this.state.isLoading &&
-            <div>
-              Ver séries do gênero:
-              {this.state.genres.map(this.renderGenreLink)}
-            </div>
-          }
-        </section>
-
-        </div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Router>
       )
     }
   
